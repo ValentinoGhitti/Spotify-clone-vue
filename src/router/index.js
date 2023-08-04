c22e55f5c8f83f4ae6138ed0cd6c552a1c0275db
valentinoghitti <valentinoghitti00@hotmail.com>
2023-08-04 04:39:58 -0300

    SearchView added

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import SearchView from '../views/SearchView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
       component: HomeView
+    },
+    {
+      path: '/search',
+      component: SearchView
     }
   ]
 })
---
+++ src/views/SearchView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Recommended radio
+    </button>
+
+    <div class="py-1.5"></div>
+
+    <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
+      <CategorySelect category="Podcasts" image="https://picsum.photos/id/7/300/300" />
+      <CategorySelect category="Audiobooks" image="https://picsum.photos/id/40/300/300" />
+      <CategorySelect category="Made for you" image="https://picsum.photos/id/13/300/300" />
+      <CategorySelect category="New Release" image="https://picsum.photos/id/17/300/300" />
+      <CategorySelect category="Podcasts" image="https://picsum.photos/id/7/300/300" />
+      <CategorySelect category="Audiobooks" image="https://picsum.photos/id/40/300/300" />
+      <CategorySelect category="Made for you" image="https://picsum.photos/id/13/300/300" />
+      <CategorySelect category="New Release" image="https://picsum.photos/id/17/300/300" />
+      <CategorySelect category="Podcasts" image="https://picsum.photos/id/7/300/300" />
+      <CategorySelect category="Audiobooks" image="https://picsum.photos/id/40/300/300" />
+      <CategorySelect category="Made for you" image="https://picsum.photos/id/13/300/300" />
+      <CategorySelect category="New Release" image="https://picsum.photos/id/17/300/300" />
+      <CategorySelect category="Podcasts" image="https://picsum.photos/id/7/300/300" />
+      <CategorySelect category="Audiobooks" image="https://picsum.photos/id/40/300/300" />
+      <CategorySelect category="Made for you" image="https://picsum.photos/id/13/300/300" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import CategorySelect from '../components/CategorySelect.vue';
+</script>
