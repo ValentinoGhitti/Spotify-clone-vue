4f0e230c99163673d26493b4b30502262a0a4fc4
valentinoghitti <valentinoghitti00@hotmail.com>
2023-08-04 04:06:44 -0300

    HomeView added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,44 @@
-<script setup>
-</script>
-
 <template>
-  <main>
-  </main>
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Podcast to make you smarter
+    </button>
+
+    <div class="py-1.5"></div>
+
+    <div class="flex items-center">
+      <HomeCard image="https://picsum.photos/id/30/300/300" title="Title is here" subTitle="Subtitle is here" />
+      <HomeCard image="https://picsum.photos/id/40/300/300" title="Title is here" subTitle="Subtitle is here" />
+      <HomeCard class="md:block hidden" image="https://picsum.photos/id/50/300/300" title="Title is here" subTitle="Subtitle is here" />
+      <HomeCard class="lg:block hidden" image="https://picsum.photos/id/60/300/300" title="Title is here" subTitle="Subtitle is here" />
+      <HomeCard class="xl:block hidden" image="https://picsum.photos/id/70/300/300" title="Title is here" subTitle="Subtitle is here" />
+    </div>
+  </div>
+
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Recommended radio
+    </button>
+
+    <div class="py-1.5"></div>
+
+    <div class="flex items-center">
+      <HomeCard image="https://picsum.photos/id/10/300/300" title="Title is here" subTitle="Subtitle is here" />
+      <HomeCard image="https://picsum.photos/id/11/300/300" title="Title is here" subTitle="Subtitle is here" />
+      <HomeCard class="md:block hidden" image="https://picsum.photos/id/77/300/300" title="Title is here" subTitle="Subtitle is here" />
+      <HomeCard class="lg:block hidden" image="https://picsum.photos/id/61/300/300" title="Title is here" subTitle="Subtitle is here" />
+      <HomeCard class="xl:block hidden" image="https://picsum.photos/id/22/300/300" title="Title is here" subTitle="Subtitle is here" />
+    </div>
+  </div>
+
 </template>
+
+<script setup>
+  import HomeCard from '../components/HomeCard.vue'
+</script>
