cb9f3e51b30475f7065dbbcd89d2221495d0c705
valentinoghitti <valentinoghitti00@hotmail.com>
2023-08-05 23:38:40 -0300

    adding new funcionts in pinia store

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,38 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useSongStore = defineStore('song', {
+  state: () => ({
+    isPlaying: false,
+    audio: null,
+    currentArtist: null,
+    currentTrack: null,
+  }),
+  
+  actions: {
+    loadSong(artist, track) {
+      this.currentArtist = artist;
+      this.currentTrack = track;
+
+      if (this.audio && this.audio.src) {
+        this.audio.pause();
+        this.isPlaying = false;
+        this.audio.src = '';
+      }
+
+      this.audio = new Audio();
+      this.audio.src = track.path;
+
+      setTimeout(() => {
+        this.isPlaying = true;
+        this.audio.play();
+      }, 200);
+    }
+
+    playOrPauseSOng() {
+      
+    }
   }
 
-  return { count, doubleCount, increment }
+
 })
