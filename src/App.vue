cc26e0f6ee4d20f4f664c749bc753852e39521c4
valentinoghitti <valentinoghitti00@hotmail.com>
2023-08-07 03:14:43 -0300

    adding Music Player Section

--- src/App.vue
+++ src/App.vue
@@ -122,17 +122,30 @@
     <RouterView />
     <div class="mb-[100px]"></div>
   </div>
+
+  <MusicPlayer v-if="currentTrack" />
 </template>
 
 <script setup>
-  import { ref } from 'vue';
+  import { ref, onMounted } from 'vue';
   import { RouterLink, RouterView } from 'vue-router';
   import MenuItem from './components/MenuItem.vue';
+  import MusicPlayer from './components/MusicPlayer.vue';
 
   import ChevronUp from 'vue-material-design-icons/ChevronUp.vue';
   import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
   import ChevronRight from 'vue-material-design-icons/ChevronRight.vue';
   import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+
+  import { useSongStore } from './stores/song';
+  import { storeToRefs } from 'pinia';
+  
+  const useSong = useSongStore();
+  const { isPlaying, currentTrack } = storeToRefs(useSong);
+
+  onMounted(() => {
+    isPlaying.value = false;
+  });
 
   let openMenu = ref(false);
 </script>
--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -1,9 +1,115 @@
 <template>
-  <div>
+  <div 
+    id="MusicPlayer"
+    v-if="audio"
+    class="
+      fixed
+      flex
+      items-center
+      justify-between
+      bottom-9
+      w-full
+      z-50
+      h-[90px]
+      bg-[#181818]
+      border-t
+      border-t-[#272727]
+    "
+  >
 
   </div>
 </template>
 
 <script setup>
   import { ref, watch, onMounted } from 'vue';
+
+  import Heart from 'vue-material-design-icons/Heart.vue';
+  import PictureInPictureBottomRight from 'vue-material-design-icons/PictureInPictureBottomRight.vue';
+  import Play from 'vue-material-design-icons/Play.vue';
+  import Pause from 'vue-material-design-icons/Pause.vue';
+  import SkipBackward from 'vue-material-design-icons/SkipBackward.vue';
+  import SkipForward from 'vue-material-design-icons/SkipForward.vue';
+
+  import { useSongStore } from '../stores/song';
+  import { storeToRefs } from 'pinia';
+  
+  const useSong = useSongStore();
+  const { 
+    isPlaying,
+    audio,
+    currentTrack,
+    currentArtist
+  } = storeToRefs(useSong);
+
+  let isHover = ref(false);
+  let isTrackTimeCurrent = ref(null);
+  let isTrackTimeTotal = ref(null);
+  let seeker = ref(null);
+  let seekerContainer = ref(null);
+  let range = ref(0);
+
+  onMounted(() => {
+    if ( audio.value ) {
+      setTimeout(() => {
+        timeupdate();
+        loadmetadata();
+      }, 300)
+    }
+
+    if ( currentTrack.value ) {
+      seeker.value.addEventListener('change', function () {
+        const time = audio.value.duration * (seeker.value.value / 100);
+        audio.value.currenTime = time;
+      });
+
+      seeker.value.addEventListener('mousedown', function () {
+        audio.value.pause();
+        isPlaying.value = false;
+      });
+
+      seeker.value.addEventListener('mouseup', function () {
+        audio.value.play();
+        isPlaying.value = true;
+      });
+
+      seekerContainer.value.addEventListener('click', function (e) {
+        const clickPosition = (e.pageX - seekerContainer.value.offsetLeft) / seekerContainer.value.offsetWidth;
+        const time = audio.value.duration * clickPosition;
+        audio.value.currenTime = time;
+        seeker.value.value = (100 / audio.value.duration) * audio.value.currenTime;
+      });
+    }
+  });
+
+  const timeupdate = () => {
+    audio.value.addEventListener('timeupdate', function () {
+      var minutes = Math.floor(audio.value.currenTime / 60);
+      var seconds = Math.floor(audio.value.currenTime - minutes * 60);
+
+      isTrackTimeCurrent.value = minutes+':'+seconds.toString().padStart(2, '0');
+      const value = (100 / audio.value.duration) * audio.value.currenTime;
+      range.value = value;
+      seeker.value = value;
+    });
+  }
+
+  const loadmetadata = () => {
+    audio.value.addEventListener('timeupdate', function () {
+      const duration = audio.value.duration;
+      const minutes = Math.floor(duration / 60);
+      const seconds = Math-floor(duration % 60);
+      isTrackTimeTotal.value = minutes+':'+seconds.toString().padStart(2, '0');
+    });
+  }
+
+  watch(() => audio.value, () => {
+    timeupdate();
+    loadmetadata();
+  });
+
+  watch(() => isTrackTimeCurrent.value, (time) => {
+    if (time && time === isTrackTimeTotal.value) {
+      useSong.nextSong(currentTrack.value);
+    }
+  });
 </script>
